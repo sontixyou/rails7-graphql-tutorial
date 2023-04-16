22c3f279c7307a484e82c830d65af1517f84934d
kenken <kengoviolinphoto@gmail.com>
2023-04-16 15:51:01 +0900

    fix typo

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,7 +1,7 @@
 module Types
   class CommentType < Types::BaseObject
     field :id, ID, null: false
-    field :post, Type::PostType, null: false
+    field :post, Types::PostType, null: false
     field :body, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
